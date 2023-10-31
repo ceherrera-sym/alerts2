bb56049ce54dfc46feaa983e94ff99fc945a177c
ceherrera-sym <40399246+ceherrera-sym@users.noreply.github.com>
2023-10-31 21:21:23 +0000

    Ajustes

--- alerts/js/alerts.js
+++ alerts/js/alerts.js
@@ -1,186 +1,230 @@
 
-
-class Alerts{
-	flotAlert(color,colorText,text,time,verticalAlign,horizontalAlign,callbak){
-		var defaultTime = 2000;
-		
-		if (time != null)  defaultTime = time;
-		$('body').prepend('<div id="flotAlert"><div></div></div>');
-		$('#flotAlert').css('background',color);
-		$('#flotAlert').css('color',colorText);
-		$('#flotAlert').css('position','fixed');
-		$('#flotAlert').css('cursor','pointer');
-		$('#flotAlert').css(''+horizontalAlign,'10px')
-		$('#flotAlert').css(''+verticalAlign,'50px')
-		$('#flotAlert div').text(text);
-		$('#flotAlert').show('slow');
-		
-		setTimeout(function() { 
-			$('#flotAlert').hide('slow');
-	       	$('#flotAlert').remove();
-			if (callbak != null){
-				callbak();
-			}
-	    }, defaultTime);
-		
-	}
 	//Alerta Simple
-	alert(container,text,aceptText,callbak){
-		
-		$(container).addClass('alert-content');
-
-		if ($('html').height() < $(window).height()) {
-			$('.alert-content').height($(window).height());
-		} else {
-			$('.alert-content').height($('html').height());
+	class CustomAlert {
+		constructor(options) {
+			this.text = options.text || '';
+			this.aceptText = options.aceptText || 'Aceptar';
+			this.callback = options.callback || null;
+			this.title = options.title || '';
+			this.createAlert();
 		}
-		$('.alert-content').html('' +
-				'<div class="alert-alert " style="display:none"> ' +
- 					'<div class="modal-content"> ' +
- 						'<div class="modal-body">' + text + '</div> ' +
-						'<div class="modal-footer"> ' +
- 						 	 '<button id="confirmButton" type="button" class="btn btn-sm btn-primary ">' + aceptText + '</button> ' +
-						'</div> ' +
-					'</div> ' +
-				'</div> ');		
-						
-		$('.alert-alert').show('fast');		
 	
-		$(document).ready(function(){ 
-			$('#confirmButton').click(function (){
-				$('.alert-alert').hide('slow');		
-				$(container).html('');
-				$(container).removeAttr('style');
-				$(container).removeAttr('class');
-			
-				if (callbak != null){
-					callbak();
+		createAlert() {
+			$('body').append('<div id="container"></div>');
+			$('#container').addClass('alert-content');
+			$('.alert-content').html(`
+				<div class="modal" id="miModal" tabindex="-1">
+					<div class="modal-dialog">
+						<div class="modal-content">
+							<div class="modal-header">
+								<h5 class="modal-title">${this.title}</h5>
+							</div>
+							<div class="modal-body">
+								<p>${this.text}.</p>
+							</div>
+							<div class="modal-footer">
+								<button type="button" id="confirmButton" class="btn btn-primary" data-bs-dismiss="modal">${this.aceptText}</button>
+							</div>
+						</div>
+					</div>
+				</div>`
+			);
+	
+			var myModal = new bootstrap.Modal(document.getElementById('miModal'));
+			myModal.toggle();
+	
+			$('#confirmButton').click(() => {
+				$('.alert-content').hide('slow');
+				$('#container').html('');
+				$('#container').remove();
+	
+				if (this.callback != null) {
+					this.callback();
 				}
-			})
-		})				
-
-		$(window).resize(function(){ 
-			if ($('html').height() < $(window).height()) {
-				$('.alert-content').height($(window).height());
-			} else {
-				$('.alert-content').height($('html').height());
-			}
-		});// End resize
-
+			});
+		}
 	}
-	//Fin alerta simple
-	//Inica confirm
-	confirm(container,text,aceptText,declineText,callbak){
-		if (callbak != null) {
-			
-			$(container).addClass('alert-content');
-			if ($('html').height() < $(window).height()) {
-				$('.alert-content').height($(window).height());
-			} else {
-				$('.alert-content').height($('html').height());
-			}
-			$('.alert-content').append('' +
-					'<div class="alert-alert " style="display:none"> ' +
-		 				'<div class="modal-content "> ' +
-		 					'<div class="modal-body">' + text + '</div> ' +
-							'<div class="modal-footer"> ' +
-		 					 	 '<button id="confirmButton" type="button" class="btn btn-sm btn-primary ">' + aceptText + '</button> ' +
-		 					 	 '<button id="canCellButton" type="button" class="btn btn-outline-secondary btn-sm ">' + declineText + '</button> ' +
-							'</div> ' +
-						'</div> ' +
-					'</div> ');
-						
-			$('.alert-alert').show('fast');		
-			
-			$(document).ready(function(){ 
-				$('#confirmButton').click(function (){
-					$('.alert-alert').hide('slow');		
-					$(container).html('');
-					$(container).height('0px');
-					callbak(true);
-				});
-				
-				$('#canCellButton').click(function (){
-					$('.alert-alert').hide('slow');		
-					$(container).html('');
-					$(container).height('0px');
-					callbak(false);
-				});
 
-				$(window).resize(function(){ 
-					if ($('html').height() < $(window).height()) {
-						$('.alert-content').height($(window).height());
-					} else {
-						$('.alert-content').height($('html').height());
+	class CustomFlotAlert {
+		constructor(options) {
+			this.color = options.color || 'info';
+			this.text = options.text || '';
+			this.time = options.time || null;
+			this.verticalAlign = options.verticalAlign || 'top';
+			this.horizontalAlign = options.horizontalAlign || 'left';
+			this.callback = options.callback || null;
+			this.execute = true;
+	
+			this.createAlert();
+		}
+	
+		createAlert() {
+			$('body').prepend(`
+				<div class="alert alert-${this.color} alert-dismissible fade shadow show" id="flotAlert" role="alert">
+					${this.text}
+					<button type="button" id="closeBtnFloatAlert" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+				</div>`);
+			
+			$('#flotAlert').css('position', 'fixed');
+			$('#flotAlert').css('cursor', 'pointer');
+			if (this.horizontalAlign == 'center') {
+				$('#flotAlert').css({
+					'left': '50%',
+					'transform': 'translateX(-50%)'
+				});
+			} else {
+				$('#flotAlert').css(`${this.horizontalAlign}`, '10px');
+			}
+			
+			if (this.verticalAlign == 'center') {
+				$('#flotAlert').css({
+					'top': '50%',
+					'transform': 'translateX(-50%)'
+				});
+			} else {
+				$('#flotAlert').css(`${this.verticalAlign}`, '50px');
+			}
+	
+			$('#closeBtnFloatAlert').click(() => {
+				$('#flotAlert').remove();
+				if (this.callback != null) {
+					this.callback();
+				}
+	
+				this.execute = false;
+			});
+	
+			if (this.time != null) {
+				setTimeout(() => {
+					if (this.execute) {
+						$('#flotAlert').remove();
+						if (this.callback != null) {
+							this.callback();
+						}
 					}
-				});// End resize
-			})
-		} else {
-			this.alert(container,'Funcion Callbak no definida','Aceptar');
+				}, this.time);
+			}
 		}
-	}
-	//termina confirm
-	//Input
-	input(container,text,type,aceptText,declineText,callbak){
-		var defaultType = 'text'; 
-		
-		if (type != null) defaultType = type;
-		
-		$(container).addClass('alert-content');
-		if ($('html').height() < $(window).height()) {
-			$('.alert-content').height($(window).height());
-		} else {
-			$('.alert-content').height($('html').height());
-		}
-		$('.alert-content').append('' +
-				'<div class="alert-alert " style="display:none"> ' +
-					'<div class="modal-content "> ' +
-						'<div class="modal-body">' +  
-							'<div class="form-group"> '+
-						    	'<label for="exampleInputEmail1">' + text + '</label>' +
- 						    	'<input type="' + defaultType + '" class="form-control" id="test"> ' +
-						 	'</div> ' +						
-						'</div> ' +						
-						'<div class="modal-footer"> ' +
-						 	 '<button id="confirmButton" type="button" class="btn btn-sm btn-primary ">' + aceptText + '</button> ' +
-		 				 	 '<button id="canCellButton" type="button" class="btn btn-outline-secondary btn-sm">' + declineText + '</button> ' +
-						'</div> ' +
-					'</div> ' +
-				'</div> ');
-						
-		$('.alert-alert').show('fast');		
-			
-		$(document).ready(function(){ 
-			var value = '';
-			$('#test').change(function(){
-				value =	$(this).val();
-			});
-		
-			$('#confirmButton').click(function (){
-				$('.alert-alert').hide('slow');		
-				$(container).html('');
-				$(container).height('0px');
-				if (callbak != null){
-					callbak(value);
-				}
-			});
-				
-			$('#canCellButton').click(function (){
-				$('.alert-alert').hide('slow');		
-				$(container).html('');
-				$(container).height('0px');
-			})
-
-			$(window).resize(function(){ 
-				if ($('html').height() < $(window).height()) {
-					$('.alert-content').height($(window).height());
-				} else {
-					$('.alert-content').height($('html').height());
-				}
-			});// End resize
-		});
-			
 	}
 	
-}
+	class CustomConfirm {
+		constructor(options) {
+			
+			this.text = options.text;
+			this.aceptText = options.aceptText || 'Aceptar';
+			this.declineText = options.declineText || 'Cancelar';
+			this.callback = options.callback ;
+	
+			if (this.callback != null) {
+				this.createConfirm();
+			} else {
+				new CustomAlert({
+					text: 'Función de Callback no definida',
+           			aceptText: 'Aceptar',
+				});
+			}
+		}
+	
+		createConfirm() {
+			$('body').append('<div id="container"></div>');
+			$('#container').addClass('alert-content');
+			$('.alert-content').html(`
+				<div class="modal" id="miModal" tabindex="-1">
+					<div class="modal-dialog">
+						<div class="modal-content">
+							<div class="modal-header">
+								<h5 class="modal-title">${this.title}</h5>
+							</div>
+							<div class="modal-body">
+								<p>${this.text}.</p>
+							</div>
+							<div class="modal-footer">
+								<button type="button" id="confirmButton" class="btn btn-primary" data-bs-dismiss="modal">${this.aceptText}</button>
+								<button type="button" id="canCellButton" class="btn btn-danger" data-bs-dismiss="modal">${this.declineText}</button>
+							</div>
+						</div>
+					</div>
+				</div>`
+			);
+	
+			var myModal = new bootstrap.Modal(document.getElementById('miModal'));
+			myModal.toggle();
+	
+			$('#confirmButton').click(() => {
+				$('#container').remove();
+				this.callback(true);
+			});
 
+			$('#canCellButton').click(() => {
+				$('#container').remove();
+				this.callback(false);
+			});
+		}
+		
+	}
+	
+
+	class CustomInput {
+		constructor(options) {
+			this.title = options.title || '';
+			this.text = options.text || '';
+			this.type = options.type || 'text';
+			this.aceptText = options.aceptText || 'Aceptar';
+			this.declineText = options.declineText || 'Cancelar';
+			this.callback = options.callback || null;
+			this.onCancel = options.onCancel || null;
+	
+			this.createInput();
+		}
+	
+		createInput() {
+			$('body').append('<div id="container"></div>');
+			$('#container').addClass('alert-content');
+			$('.alert-content').html(`
+				<div class="modal" id="miModal" tabindex="-1">
+					<div class="modal-dialog">
+						<div class="modal-content">
+							<div class="modal-header">
+								<h5 class="modal-title">${this.title}</h5>
+							</div>
+							<div class="modal-body">
+								<div class="form-group"> 
+								<label for="exampleInputEmail1">` + this.text + `</label>
+								<input type="` + this.type + `" class="form-control" id="inputAlert"> 
+							</div> 
+							</div>
+							<div class="modal-footer">
+								<button type="button" id="confirmButton" class="btn btn-primary" data-bs-dismiss="modal">${this.aceptText}</button>
+								<button type="button" id="canCellButton" class="btn btn-danger" data-bs-dismiss="modal">${this.declineText}</button>
+							</div>
+						</div>
+					</div>
+				</div>`
+			);
+
+			var myModal = new bootstrap.Modal(document.getElementById('miModal'));
+			myModal.toggle();
+	
+			var value = '';
+	
+			$('#inputAlert').change(function(){
+				value = $(this).val();
+			});
+	
+			$('#confirmButton').click(() => {
+				$('#container').remove();
+				if (this.callback != null) {
+					this.callback(value);
+				}
+			});
+	
+			$('#canCellButton').click(() => {
+				$('#container').remove();
+					if (this.onCancel != null) {
+					this.onCancel();
+				}
+			});
+	
+		}
+	}
+	
